8adcbad98efdb022e66609e14acae22be1db1067
XWang1024 <xwang1024@126.com>
2017-01-12 16:14:32 +0800

    首页走马灯合入

--- src/home/containers/HomeContainer.js
+++ src/home/containers/HomeContainer.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import "home/resource/index.less";
 
 import Slogan from '../components/Slogan';
+import CarouselContainer from './CarouselContainer';
 import GoodCourses from '../components/GoodCourses';
 
 
@@ -39,8 +40,8 @@ class HomeContainer extends Component {
                 <div className="slogan">
                     <Slogan />
                 </div>
-                <div className="carousel">
-
+                <div className="carousel-wrapper">
+                    <CarouselContainer />
                 </div>
                 <div className="good-courses">
                     <GoodCourses />

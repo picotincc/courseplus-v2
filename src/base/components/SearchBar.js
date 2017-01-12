c94278f1a536bbcaefa76ae641f5df65d44eddfe
maybeShuo <546537825@qq.com>
2017-01-12 15:25:37 +0800

    新建搜索组件。

---
+++ src/base/components/SearchBar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+
+    constructor (props) {
+        super(props);
+    }
+
+    static defaultProps = {
+
+    }
+
+    static propTypes = {
+
+    }
+
+    state = {
+
+    }
+
+    componentDidMount()
+    {
+
+    }
+
+    render()
+    {
+        return (
+            <div className="cp-search-bar">
+                <div className="title">
+                    <span>考研目标</span>
+                </div>
+                <div className="title">
+                    <span>考研目标</span>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ src/base/components/SearchInput.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+export default class SearchInput extends Component {
+
+    constructor (props) {
+        super(props);
+    }
+
+    static defaultProps = {
+
+    }
+
+    static propTypes = {
+
+    }
+
+    state = {
+
+    }
+
+    componentDidMount()
+    {
+
+    }
+
+    render()
+    {
+        return (
+            <div className="cp-search-input">
+            </div>
+        );
+    }
+}

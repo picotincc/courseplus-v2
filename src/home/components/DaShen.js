4793d2abbb70c38297ce375debedcfe13b2c49d3
chuchengbo <15656205870@163.com>
2017-01-12 15:26:04 +0800

    test1

--- src/home/components/DaShen.js
+++ src/home/components/DaShen.js
@@ -32,6 +32,7 @@ export default class DaShen extends Component {
       function onChange(a, b, c) {
          console.log(a, b, c);
        };
+
       return (
              <div className="cp-home-dashen">
                <p className="title">大神入驻</p>
@@ -65,24 +66,5 @@ export default class DaShen extends Component {
                 </Carousel>
 
           </div>
-
-
-
-    //   <div className="cp-home-dashen">
-    //     <p className="title">大神入驻</p>
-    //     {daLists.map(item => {
-    //       return (
-    //         <div className="item" key={item}>
-    //           <p><img className="item-img" src={imgSrc}/></p>
-    //           <p className="item-name">李刚生</p>
-    //           <p className="item-title">南京大学企业管理专业一专业课18</p>
-    //           <p className="item-detail">曾任教多家考研机构VIP一对一辅导</p>
-    //           <p className="item-detail">熟悉研究多年专业考研真题，结合自身考研实战经验，对管理学框架进行更适应考试的搭建及运用</p>
-    //         </div>
-    //     )
-    //     })
-    //   }
-    // </div>
-  );
-    }
+        )}
 }

580382bdc85315c421f5e7d47263b3705cea532e
chuchengbo <15656205870@163.com>
2017-01-13 15:41:31 +0800

    merge

--- mock_data/contributors.js
+++ mock_data/contributors.js
@@ -9,23 +9,23 @@ const data = {
                       "count": 0,
                       "list": [
                                 {
-                                  "id": 0,
-                                  "name": "string",
-                                  "img_url": "string",
-                                  "account": "string",
+                                  "id": 1,
+                                  "name": "李更生",
+                                  "img_url": "http://ojnq0puar.bkt.clouddn.com/banner.png",
+                                  "account": "阿萨德看了就爱上考",
                                   "gender": 0,
-                                  "introduction": "string",
-                                  "description": "string",
-                                  "education": "string"
+                                  "introduction": "阿克苏豆角啊看牢圣诞节快乐啊看牢神盾局阿萨德",
+                                  "description": "阿萨德阿萨德阿萨德阿萨德阿萨德阿达 请问额请问请问请问饿 阿萨德爱迪生",
+                                  "education": "南京大学"
                                 },{
-                                  "id": 0,
-                                  "name": "string",
-                                  "img_url": "string",
-                                  "account": "string",
+                                  "id": 2,
+                                  "name": "教科文",
+                                  "img_url": "http://ojnq0puar.bkt.clouddn.com/banner.png",
+                                  "account": "爱是看得见",
                                   "gender": 0,
-                                  "introduction": "string",
-                                  "description": "string",
-                                  "education": "string"
+                                  "introduction": "而快去问了破千万饿哦平去哦排位",
+                                  "description": "我我欧赔温柔iew肉皮 轻微哦请问",
+                                  "education": "东南大学"
                                 }
                               ]
                     }

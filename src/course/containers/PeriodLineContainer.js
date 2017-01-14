b6fc6f27034c31d211ba7a058cb9d7c776f08d2f
XWang1024 <xwang1024@126.com>
2017-01-14 14:14:47 +0800

    课时时间轴合入

--- src/course/containers/PeriodLineContainer.js
+++ src/course/containers/PeriodLineContainer.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { getPeriods } from 'base/actions/CourseAction';
+import { getPeriods, selectPeriod } from 'base/actions/CourseAction';
+import PeriodLine from "../components/PeriodLine";
 
 class PeriodLineContainer extends Component {
 
@@ -26,10 +27,14 @@ class PeriodLineContainer extends Component {
         dispatch(getPeriods(1));
     }
 
+    handleSelectPeriod(period) {
+        let { dispatch } = this.props;
+        dispatch(selectPeriod(period));
+    }
+
     render() {
         return (
-            <div className="">
-            </div>
+            <PeriodLine periods={this.props.coursePeriods} handleSelectPeriod={this.handleSelectPeriod.bind(this)}/>
         );
     }
 }

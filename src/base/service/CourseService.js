e7f75221b3e9bc53abd3669d2d48e7bfff78e57e
luckymore <njuluckwang@gmail.com>
2017-01-14 16:49:07 +0800

    把获取首页评论的接口请求放到了CourseService中

--- src/base/service/CourseService.js
+++ src/base/service/CourseService.js
@@ -4,5 +4,6 @@ import fetch from './FetchWrapper';
 const version = '/v1';
 
 export default {
-    getPeriods: (course_id) => fetch(`${version}/course/period`, { query: { course_id } })
+    getPeriods: (course_id) => fetch(`${version}/course/period`, { query: { course_id } }),
+    getQualityComments: () => fetch(`${version}/course/qualityComment`)
 }

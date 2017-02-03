1448e7c379914cb806e156af65261141cbe79f62
luckymore <njuluckwang@gmail.com>
2017-02-03 10:56:19 +0800

    丢失的teacher.js

---
+++ mock_data/teacher.js
@@ -0,0 +1,74 @@
+const data = {
+    "/v1/teacher/qualityTeacher": {
+        GET: {
+              "data":[
+                      {
+                        "id": 1,
+                        "name": "Jessie",
+                        "img_url": "http://ojnq0puar.bkt.clouddn.com/banner.png",
+                        "account": "南京大学企业管理专业研一 专业课119",
+                        "gender": 0,
+                        "introduction": "曾任教多家考研机构VIP一对一辅导",
+                        "description": "熟悉研究多年专业考研真题，结合自身考研实战经验，对管理学框架进行更适应考试的搭建及运用",
+                        "education": "南京大学"
+                      },{
+                        "id": 2,
+                        "name": "李港生",
+                        "img_url": "http://ojnq0puar.bkt.clouddn.com/banner.png",
+                        "account": "南京大学法律硕士（非法学）专业研一 专业基础课 127 专业综合课 130",
+                        "gender": 0,
+                        "introduction": "考研经验分享曾被法硕联盟论坛精选推送",
+                        "description": "研究历年真题、南大老师讲课音频，准确把握命题风格及重点,对管理学框架进行更适应考试的搭建及运用",
+                        "education": "东南大学"
+                        },{
+                          "id": 3,
+                          "name": "杨亚男",
+                          "img_url": "http://ojnq0puar.bkt.clouddn.com/banner.png",
+                          "account": "南京大学法律硕士（非法学）专业研一 专业基础课 127 专业综合课 130",
+                          "gender": 0,
+                          "introduction": "考研经验分享曾被法硕联盟论坛精选推送",
+                          "description": "研究历年真题、南大老师讲课音频，准确把握命题风格及重点",
+                          "education": "东南大学"
+                        },{
+                          "id": 4,
+                          "name": "贾宝玉",
+                          "img_url": "http://ojnq0puar.bkt.clouddn.com/banner.png",
+                          "account": "南京大学法律硕士（非法学）专业研一 专业基础课 127 专业综合课 130",
+                          "gender": 0,
+                          "introduction": "考研经验分享曾被法硕联盟论坛精选推送,考研经验甚是独特",
+                          "description": "研究历年真题、南大老师讲课音频，准确把握命题风格及重点",
+                          "education": "东南大学"
+                        },{
+                          "id": 5,
+                          "name": "李港生",
+                          "img_url": "http://ojnq0puar.bkt.clouddn.com/banner.png",
+                          "account": "南京大学法律硕士（非法学）专业研一 专业基础课 127 专业综合课 130",
+                          "gender": 0,
+                          "introduction": "考研经验分享曾被法硕联盟论坛精选推送",
+                          "description": "研究历年真题、南大老师讲课音频，准确把握命题风格及重点",
+                          "education": "东南大学"
+                        },{
+                          "id": 6,
+                          "name": "李港生",
+                          "img_url": "http://ojnq0puar.bkt.clouddn.com/banner.png",
+                          "account": "南京大学法律硕士（非法学）专业研一 专业基础课 127 专业综合课 130",
+                          "gender": 0,
+                          "introduction": "考研经验分享曾被法硕联盟论坛精选推送",
+                          "description": "研究历年真题、南大老师讲课音频，准确把握命题风格及重点",
+                          "education": "东南大学"
+                        },{
+                          "id": 7,
+                          "name": "李港生",
+                          "img_url": "http://ojnq0puar.bkt.clouddn.com/banner.png",
+                          "account": "南京大学法律硕士（非法学）专业研一 专业基础课 127 专业综合课 130",
+                          "gender": 0,
+                          "introduction": "考研经验分享曾被法硕联盟论坛精选推送",
+                          "description": "研究历年真题、南大老师讲课音频，准确把握命题风格及重点",
+                          "education": "东南大学"
+                        }
+                  ]
+        }
+    }
+}
+
+module.exports = data;

38e76572a3ddd99a1ec115050e0e03e97643b073
maybeShuo <546537825@qq.com>
2017-02-04 11:26:17 +0800

    跳转首页

--- src/base/containers/NotFoundContainer.js
+++ src/base/containers/NotFoundContainer.js
@@ -6,6 +6,7 @@ export default class NotFoundContainer extends Component {
 
     constructor (props) {
         super(props);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     static defaultProps = {
@@ -25,11 +26,16 @@ export default class NotFoundContainer extends Component {
 
     }
 
+    handleClick()
+    {
+        location.href = "/";
+    }
+
     render()
     {
         return (
             <div className="cp-notfound-container">
-                <div className="home">
+                <div className="home" onClick={this.handleClick}>
                     <span className="icon iconfont icon-home"></span>
                     <span className="text">返回首页</span>
                 </div>
